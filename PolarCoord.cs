179ee8b8bc7a2fbbc1e1269d819579b96eb996b3
calmandniceperson <mko.mko31@gmail.com>
2016-08-21 18:06:00 +0200

    Add set to Angle property

--- PolarCoord.cs
+++ PolarCoord.cs
@@ -3,7 +3,7 @@ namespace AirTrafficControl
     public class PolarCoord
     {
         public double Distance {get;set;}
-        public double Angle {get;}
+        public double Angle {get;set;}
 
         public PolarCoord(double distance, double angle)
         {
